2ea6cf1516de291e88f71e35aa25c6a3126c7269
isko18 <toksonbaevislam2004@gmail.com>
2025-12-25 03:28:14 +0600

    first commit

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+spa_view = TemplateView.as_view(template_name="index.html")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # API
     path("api/", include("offers.urls")),
+
+    # Главная (Vite dist index.html)
+    path("", spa_view),
+]
+
+# SPA fallback (React Router refresh)
+urlpatterns += [
+    re_path(r"^(?!api/|admin/|static/|media/).*$", spa_view),
 ]
 
 if settings.DEBUG:
